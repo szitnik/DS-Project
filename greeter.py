7983f712e31aba2c0650454c2a70b0ce45221c94
Slavko <slavko@zitnik.si>
2020-08-10 15:47:08 +0200

    Track and uni added

--- greeter.py
+++ greeter.py
@@ -1,6 +1,10 @@
 def main():
+  print("###################################\nSTUDENT ENROLLMENT FORM\n###################################")
+
   student = input("Please enter your name: ")
+  uni = input("Please enter your University name: ")
+  track = input("Please enter your study programme: ")
   
-  print(f"Dear {student}, we are happy to have you in the Data Science track at the University of Ljubljana!")
+  print(f"Dear {student}, we are happy to have you in the {track} track at the {uni}!")
   
 main()
