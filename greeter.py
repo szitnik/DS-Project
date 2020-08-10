d263b018a99189aab0727989ed5db35c9d3c383e
Slavko <slavko@zitnik.si>
2020-08-10 14:52:34 +0200

    Uni. Ljubljana added.

--- greeter.py
+++ greeter.py
@@ -1,6 +1,6 @@
 def main():
   student = input("Please enter your name: ")
   
-  print(f"Dear {student}, we are happy to have you in the Data Science track!")
+  print(f"Dear {student}, we are happy to have you in the Data Science track at the University of Ljubljana!")
   
 main()
